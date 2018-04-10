9aec24a79a0ffa57ce67583f866046b577d27392
Kenneth Au <kennethau82@gmail.com>
2018-04-10 22:17:51 +1000

    Added Showcard package.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Landing from "./components/Landing";
-import { HashRouter, Route } from "react-router-dom";
+import Search from "./components/Search";
+import { BrowserRouter, Route } from "react-router-dom";
 
 class App extends Component {
 	render() {
 		return (
-			<HashRouter>
+			<BrowserRouter>
 				<div className="App">
 					<header className="App-header">
 						<img src={logo} className="App-logo" alt="logo" />
 						<h1 className="App-title">Welcome to React</h1>
 					</header>
 					<Route exact path="/" component={Landing} />
+					<Route path="/search" component={Search} />
 				</div>
-			</HashRouter>
+			</BrowserRouter>
 		);
 	}
 }
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
 	return (
 		<div className="landing">
 			<h1>Landing here</h1>
+			<Link to="/search"> or Browse All </Link>
 		</div>
 	);
 };
